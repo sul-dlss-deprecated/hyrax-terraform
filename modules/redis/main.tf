491b5f267a3738e856edaff6acde438efa347572
Jon Robertson <jonrober@stanford.edu>
2017-10-26 14:17:46 -0700

    Added test for redis module, along with fleshing out

--- modules/redis/main.tf
+++ modules/redis/main.tf
@@ -1,11 +1,29 @@
+# Create a security group allowing a webapp access to the redis port.
+resource "aws_security_group" "redis" {
+  vpc_id = "${var.vpc_id}"
+  name   = "Redis security group"
+
+  ingress {
+    security_groups = ["${var.WebappSecurityGroup}"]
+    from_port       = 6379
+    to_port         = 6379
+    protocol        = "tcp"
+  }
+
+  tags {
+    Name = "${var.StackName}-redis"
+  }
+}
+
+# Create the actual redis
 resource "aws_elasticache_replication_group" "redis" {
   node_type                     = "${var.InstanceType}"
-  subnet_group_name             = "${aws_elasticache_subnet_group.redis_subnet_group.id}"
-  security_group_ids            = ["${aws_security_group.redis_security_group.id}"]
-  number_cache_clusters         = "${var.NumCacheNodes}"
+  subnet_group_name             = "${var.SubnetID}"
+  security_group_ids            = ["${aws_security_group.redis.id}"]
+  number_cache_clusters         = "1"
 
   # Not in the cloudformation config, but required by Terraform.
   replication_group_id          = "hyku-redis-cluster"
   replication_group_description = "Hyku redis stack using ElastiCache"
-  port                          = 6379
+  port                          = "6379"
 }
--- modules/redis/variables.tf
+++ modules/redis/variables.tf
@@ -1,15 +1,34 @@
-variable SecurityGroups {
-  type        = "commadelimitedlist"
-  description = "A list of security group ids, such as sg-a123fd85"
-}
+# Removing - setting directly as part of module, leaving commented out until
+#  migration complete to make it clear that this is something we are
+#  deliberately removing.
+#variable SecurityGroups {
+#  type        = "commadelimitedlist"
+#  description = "A list of security group ids, such as sg-a123fd85"
+#}
 
 variable SubnetID {
-  type        = "list"
+  type        = "string"
   description = "List of an existing subnet IDs to use for the load balancer and auto"
 }
 
 variable InstanceType {
   type        = "string"
   description = "The cache instance type"
+  default     = "cache.m1.small"
 }
 
+variable StackName {
+  type        = "string"
+  description = "Name of the environment"
+}
+
+variable WebappSecurityGroup {
+  type        = "string"
+  description = "Security group to be granted redis access"
+}
+
+## Temp until vpc done.
+variable vpc_id {
+  type    = "string"
+  default = "vpc-d84467b3"
+}

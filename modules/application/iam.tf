bd91ca4e045dccd203d278158f03ec90ea0aee5a
Jon Robertson <jonrober@stanford.edu>
2017-10-31 15:26:49 -0700

    Add the application stack
    Testing for this is pending its requirements of the worker and webapp
    modules.

---
+++ modules/application/iam.tf
@@ -0,0 +1,43 @@
+data "aws_iam_policy_document" "application_assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "application_s3" {
+  statement {
+    actions   = ["s3:*"]
+    resources = ["arn:aws:s3:::${aws_s3_bucket.upload.name}"]
+  }
+}
+
+resource "aws_iam_policy" "application" {
+  name   = "application-policy"
+  role   = "${aws_iam_role.application.id}"
+  policy = "${data.aws_iam_policy_document.application_s3}"
+}
+
+resource "aws_iam_role" "application" {
+  name               = "application-role"
+  path               = "/"
+  assume_role_policy = "${data.aws_iam_policy_document.application_assume_role.json}"
+}
+
+resource "aws_iam_role_policy_attachment" "application-webtier" {
+  role       = "${aws_iam_role.application.name}"
+  policy_arn = "arn:aws:iam::aws:policy/AWSElasticBeanstalkWebTier"
+}
+
+resource "aws_iam_role_policy_attachment" "application-workertier" {
+  role       = "${aws_iam_role.application.name}"
+  policy_arn = "arn:aws:iam::aws:policy/AWSElasticBeanstalkWorkerTier"
+}
+
+resource "aws_iam_instance_profile" "application" {
+  name = "application-profile"
+  role = "${aws_iam_role.application.name}"
+}

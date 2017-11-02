a075728fa18cf918dc38cdeefa00feab300c6e14
Erin Fahy <efahy@stanford.edu>
2017-11-01 17:53:17 -0700

    Initial commit for solr module

---
+++ modules/solr/dns.tf
@@ -0,0 +1,13 @@
+data "aws_route53_zone" "selected" {
+  name = "${var.hosted_zone_name}."
+  private_zone = true
+}
+
+resource "aws_route53_record" "solr" {
+  name = "solr.${data.aws_route53_zone.selected.name}"
+
+  zone_id = "${data.aws_route53_zone.selected.zone_id}"
+  type    = "CNAME"
+  ttl     = "900"
+  records = ["${aws_elastic_beanstalk_environment.solr.cname}"]
+}

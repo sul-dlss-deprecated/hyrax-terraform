e1a63b0be98d425f363fdf681b3830aa93771e4e
Erin Fahy <efahy@stanford.edu>
2017-11-06 12:10:14 -0600

    Security groups for solrcloud

--- securitygroups.tf
+++ securitygroups.tf
@@ -41,3 +41,104 @@ resource "aws_security_group" "fedora" {
     Name = "${var.StackName}-fcrepo"
   }
 }
+
+resource "aws_security_group" "solr" {
+  vpc_id = "${module.hyrax_vpc.vpc_id}"
+  name   = "Solr security group"
+
+  ingress {
+    from_port = 0
+    to_port   = 65535
+    protocol  = "tcp"
+    self      = true
+  }
+
+  ingress {
+    from_port       = 8983
+    to_port         = 8983
+    protocol        = "tcp"
+    security_groups = ["${aws_security_group.solr_lb.id}"]
+  }
+
+  tags {
+    Name = "${var.StackName}-solr"
+  }
+}
+
+resource "aws_security_group" "solr_lb" {
+  vpc_id = "${module.hyrax_vpc.vpc_id}"
+  name   = "Solr LB security group"
+
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = ["${aws_security_group.webapp.id}"]
+  }
+
+  tags {
+    Name = "${var.StackName}-solr-lb"
+  }
+}
+
+resource "aws_security_group" "solr_to_lb" {
+  vpc_id = "${module.hyrax_vpc.vpc_id}"
+  name   = "Solr instance to LB security group"
+
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = ["${aws_security_group.solr.id}"]
+  }
+
+  tags {
+    Name = "${var.StackName}-solr-to-lb"
+  }
+}
+
+resource "aws_security_group" "zookeeper_lb" {
+  name   = "Zookeeper LB security group"
+  vpc_id = "${module.hyrax_vpc.vpc_id}"
+
+  ingress {
+    from_port       = 2181
+    to_port         = 2181
+    protocol        = "tcp"
+    security_groups = ["${aws_security_group.solr.id}","${aws_security_group.webapp.id}"]
+  }
+
+  tags {
+    Name = "${var.StackName}-zookeeper-lb"
+  }
+}
+
+resource "aws_security_group" "zookeeper" {
+  name   = "Zookeeper security group"
+  vpc_id = "${module.hyrax_vpc.vpc_id}"
+
+  ingress {
+    from_port       = 2181
+    to_port         = 2181
+    protocol        = "tcp"
+    security_groups = ["${aws_security_group.zookeeper_lb.id}","${aws_security_group.solr.id}"]
+  }
+
+  ingress {
+    from_port       = 8181
+    to_port         = 8181
+    protocol        = "tcp"
+    security_groups = ["${aws_security_group.zookeeper_lb.id}"]
+  }
+
+  ingress {
+    from_port = 0
+    to_port   = 65535
+    protocol  = "tcp"
+    self      = true
+  }
+
+  tags {
+    Name = "${var.StackName}-zookeeper"
+  }
+}

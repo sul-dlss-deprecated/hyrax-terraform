f3bb2b9b2a073f8fc83a3fa9d8ea449561779bff
Erin Fahy <efahy@stanford.edu>
2017-11-07 12:45:44 -0600

    Use the speical beanstalk thing for timeouts

--- modules/zookeeper/single_docker_beanstalk.tf
+++ modules/zookeeper/single_docker_beanstalk.tf
@@ -14,10 +14,7 @@ resource "aws_elastic_beanstalk_environment" "zookeeper" {
   version_label       = "${var.version_label}"
   solution_stack_name = "${data.aws_elastic_beanstalk_solution_stack.single_docker.name}"
 
-  timeouts {
-    create = "30m"
-    update = "30m"
-  }
+  wait_for_ready_timeout = "30m"
 
   setting {
     namespace = "aws:autoscaling:asg"

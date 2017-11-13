1d85b482b2e235b4c8b9ea7a9b5217d1c8a1b9b6
Jon Robertson <jonrober@stanford.edu>
2017-11-13 10:03:48 -0800

    Fix SubnetID as list in the bastion module

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ module "hyrax_bastion" {
   vpc_id         = "${module.hyrax_vpc.vpc_id}"
   region         = "${var.region}"
   StackName      = "${var.StackName}"
-  SubnetID       = ["${module.hyrax_vpc.public_subnets}"]
+  SubnetID       = "${module.hyrax_vpc.public_subnets}"
   SecurityGroups = ["${list(aws_security_group.bastion.id)}"]
   KeyName        = "${var.KeyName}"
   InstanceType   = "${var.BastionInstanceType}"
